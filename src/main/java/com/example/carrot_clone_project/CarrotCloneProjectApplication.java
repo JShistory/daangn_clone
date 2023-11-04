88cb4eb999f3caeb407f4d2fb2629f73235121e9
JShistory <behappy0916@naver.com>
2023-11-04 16:26:18 +0900

    init project

---
+++ src/main/java/com/example/carrot_clone_project/CarrotCloneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrot_clone_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotCloneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotCloneProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carrot_clone_project/CarrotCloneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrot_clone_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotCloneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
